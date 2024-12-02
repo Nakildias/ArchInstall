147e5b0ef7e932e3dba344a97a01274baf91a3c7
Nakildias <130524745+Nakildias@users.noreply.github.com>
2024-12-02 18:36:06 -0500

    Update ArchInstallAssistantP2.sh

--- ArchInstallAssistantP2.sh
+++ ArchInstallAssistantP2.sh
@@ -92,6 +92,9 @@
   echo "copying root config to $username"
   mkdir /home/nakildias
   cp -rf ~/.* /home/$username/
+  chmod +rwx /home/$username/*
+  chmod +rwx /home/$username/.*
+  sed -i 8s/.*/export OSH='/home/$username/.oh-my-bash'/ /home/nakildias/.bashrc
   echo "More bash themes can be found at default for this install is [lamba]"
   echo "https://github.com/ohmybash/oh-my-bash/tree/master/themes"
   echo "Theme config is located at ~/.bashrc line #12"
