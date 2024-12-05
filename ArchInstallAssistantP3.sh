b2e94cb6be231528e90564817416ef7d033b7a7f
Nakildias <130524745+Nakildias@users.noreply.github.com>
2024-12-04 21:08:44 -0500

    Update ArchInstallAssistantP3.sh

--- ArchInstallAssistantP3.sh
+++ ArchInstallAssistantP3.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Welcome to Arch Install Assistant (Part 3/3)"
+echo "Cleaning Scripts"
+rm -rf /mnt/Grub-Themes
+rm /mnt/ArchInstallAssistantP2.sh
+
 echo "Unmounting Everything"
 umount -R /mnt
-echo "Installation finsished..."
-read -p "PRESS ENTER TO SHUTDOWN"
+read -p "Press [Enter] to complete the installation."
 shutdown now
